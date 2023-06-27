dc73c0f39ae376691a1f34c1e254598e89f5a9cd
Stephane DUFOUR <dufstef@hotmail.fr>
2023-06-27 14:38:08 +0200

    first commit

---
+++ src/Controller/AuteurController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AuteurRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuteurController extends AbstractController
+{
+    /**
+     * Pour avoirune route dans un projet symfony, on utilise les attributs PHP 8
+     * (ou les annotations si on utilise PHP 7)
+     * Le constructeur de la classe Route a un argument obligatoire, le chemin relatif,
+     * c'est-à-dire l'URL pour lequel cette méthode sera exécutée.
+     * ⚠ le chemin dtoi tjs commencer par "/".
+     */
+    #[Route('/auteur', name: 'app_auteur')]
+    public function index(AuteurRepository $auteurRepository): Response
+    {
+        /**
+         * La méthode render() vas générer l'affichage ;
+         * 1er argument : le fichier qui vas être utilisé par l'affichage
+         * 2ème arguement (optionel) : il doit être de type array. Il contient
+         *              les variables qui seront utilisés dans le fichier utilisé pour
+         *              l'affichage. C'est un array ASSOCIATIF
+         * 
+         * NB : le nom du fichier est donné a partir  du dossier "template" 
+         * 
+         */
+
+        $listeAuteur = $auteurRepository->findAll() ;
+
+        //dd($listeAuteur) ;
+        return $this->render('auteur/index.html.twig', [
+            "auteurs" => $listeAuteur
+        ]);
+    }
+
+    #[Route('/auteur/test', name: 'app_auteur_test')]
+    public function test()
+    {
+        return $this->render("base.html.twig", ["variable" => 75, "nombre" => 2, "prenom" => "Francois"]);
+    }
+
+    #[Route('/auteur/salut', name: 'app_auteur_salut')]
+    public function salut()
+    {
+        return $this->render("auteur/salut.html.twig", ["prenom" => "stef"]);
+    }
+
+}
---
+++ src/Entity/Auteur.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuteurRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuteurRepository::class)]
+class Auteur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $bio = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $naissance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getBio(): ?string
+    {
+        return $this->bio;
+    }
+
+    public function setBio(?string $bio): static
+    {
+        $this->bio = $bio;
+
+        return $this;
+    }
+
+    public function getNaissance(): ?\DateTimeInterface
+    {
+        return $this->naissance;
+    }
+
+    public function setNaissance(?\DateTimeInterface $naissance): static
+    {
+        $this->naissance = $naissance;
+
+        return $this;
+    }
+}
